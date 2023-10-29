adbce846fc16ec2fd2bf47f6f09e7bd6d34200cc
AJ Markow <alexanderjmarkow@gmail.com>
2023-10-29 01:56:28 -0500

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  mode: "univeral",
+  target: "static",
   modules: [
     "@nuxtjs/supabase",
     "@nuxtjs/tailwindcss",
